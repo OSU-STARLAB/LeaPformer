OSU-STARLAB/LeaPformer

commit a1e262369e50486a72181dd93d01fc49c78d709f

refactor build_generator to reduce code duplication. (#2716)
Summary:
# Before submitting

- [x] Was this discussed/approved via a Github issue? (no need for typos, doc improvements)
- [x] Did you read the [contributor guideline](https://github.com/pytorch/fairseq/blob/master/CONTRIBUTING.md)?
- [ ] Did you make sure to update the docs?
- [ ] Did you write any new necessary tests?

## What does this PR do?
Fixes https://github.com/pytorch/fairseq/issues/2693

## PR review
Anyone in the community is free to review the PR once the tests have passed.
If we didn't discuss your PR in Github issues there's a high chance it will not be merged.

## Did you have fun?
Make sure you had fun coding �

Pull Request resolved: https://github.com/pytorch/fairseq/pull/2716

Reviewed By: kahne

Differential Revision: D24243384

Pulled By: myleott

fbshipit-source-id: cdf4fb3b97d87dd8dbb0ea7cdb5f286277892d81

fairseq-leapformer/examples/speech_recognition/infer.py
@@ -244,6 +244,7 @@ def main(args, task=None, model_state=None):
         # Load dataset splits
         task = tasks.setup_task(args)
         task.load_dataset(args.gen_subset)
+
         logger.info(
             "| {} {} {} examples".format(
                 args.data, args.gen_subset, len(task.dataset(args.gen_subset))
@@ -281,24 +282,7 @@ def main(args, task=None, model_state=None):
     # Initialize generator
     gen_timer = StopwatchMeter()
 
-    def build_generator(args):
-        w2l_decoder = getattr(args, "w2l_decoder", None)
-        if w2l_decoder == "viterbi":
-            from examples.speech_recognition.w2l_decoder import W2lViterbiDecoder
-
-            return W2lViterbiDecoder(args, task.target_dictionary)
-        elif w2l_decoder == "kenlm":
-            from examples.speech_recognition.w2l_decoder import W2lKenLMDecoder
-
-            return W2lKenLMDecoder(args, task.target_dictionary)
-        elif w2l_decoder == "fairseqlm":
-            from examples.speech_recognition.w2l_decoder import W2lFairseqLMDecoder
-
-            return W2lFairseqLMDecoder(args, task.target_dictionary)
-        else:
-            return super().build_generator(args)
-
-    generator = build_generator(args)
+    generator = task.build_generator(models, args)
 
     if args.load_emissions:
         generator = ExistingEmissionsDecoder(

fairseq-leapformer/examples/speech_recognition/tasks/speech_recognition.py
@@ -123,6 +123,10 @@ class SpeechRecognitionTask(LegacyFairseqTask):
             from examples.speech_recognition.w2l_decoder import W2lKenLMDecoder
 
             return W2lKenLMDecoder(args, self.target_dictionary)
+        elif w2l_decoder == "fairseqlm":
+            from examples.speech_recognition.w2l_decoder import W2lFairseqLMDecoder
+
+            return W2lFairseqLMDecoder(args, self.target_dictionary)
         else:
             return super().build_generator(models, args)
 

commit 584359263f71f151351028eba6179a2bddc04bda

fix infer.py (#1354)
Summary:
this fixes infer.py that was broken by #2716 / D24243384 (https://github.com/pytorch/fairseq/commit/a1e262369e50486a72181dd93d01fc49c78d709f)

we should prob add some tests for infer.py.

Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1354

Reviewed By: myleott

Differential Revision: D24381516

Pulled By: alexeib

fbshipit-source-id: b49e6bed7d239a55b8536d13c75fd5287330b1b1

fairseq-leapformer/examples/speech_recognition/infer.py
@@ -38,13 +38,16 @@ def add_asr_eval_argument(parser):
         help="wfstlm on dictonary\
 output units",
     )
-    parser.add_argument(
-        "--lm-weight",
-        "--lm_weight",
-        type=float,
-        default=0.2,
-        help="weight for lm while interpolating with neural score",
-    )
+    try:
+        parser.add_argument(
+            "--lm-weight",
+            "--lm_weight",
+            type=float,
+            default=0.2,
+            help="weight for lm while interpolating with neural score",
+        )
+    except:
+        pass
     parser.add_argument(
         "--rnnt_len_penalty", default=-0.5, help="rnnt length penalty on word level"
     )
@@ -282,7 +285,25 @@ def main(args, task=None, model_state=None):
     # Initialize generator
     gen_timer = StopwatchMeter()
 
-    generator = task.build_generator(models, args)
+    def build_generator(args):
+        w2l_decoder = getattr(args, "w2l_decoder", None)
+        if w2l_decoder == "viterbi":
+            from examples.speech_recognition.w2l_decoder import W2lViterbiDecoder
+
+            return W2lViterbiDecoder(args, task.target_dictionary)
+        elif w2l_decoder == "kenlm":
+            from examples.speech_recognition.w2l_decoder import W2lKenLMDecoder
+
+            return W2lKenLMDecoder(args, task.target_dictionary)
+        elif w2l_decoder == "fairseqlm":
+            from examples.speech_recognition.w2l_decoder import W2lFairseqLMDecoder
+
+            return W2lFairseqLMDecoder(args, task.target_dictionary)
+        else:
+            print('only wav2letter decoders with (viterbi, kenlm, fairseqlm) options are supported at the moment')
+
+    # please do not touch this unless you test both generate.py and infer.py with audio_pretraining task
+    generator = build_generator(args)
 
     if args.load_emissions:
         generator = ExistingEmissionsDecoder(