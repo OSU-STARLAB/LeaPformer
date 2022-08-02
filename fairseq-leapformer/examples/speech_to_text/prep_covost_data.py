Add global cepstral normalization for covost & minor fix for edge case

fairseq-leapformer/examples/speech_to_text/prep_covost_data.py
@@ -11,6 +11,7 @@ import shutil
 from tempfile import NamedTemporaryFile
 from typing import Optional, Tuple
 
+import numpy as np
 import pandas as pd
 import torchaudio
 import multiprocessing
@@ -25,6 +26,7 @@ from examples.speech_to_text.data_utils import (
     get_zip_manifest,
     load_df_from_tsv,
     save_df_to_tsv,
+    cal_gcmvn_stats,
 )
 from torch import cat
 from torch import Tensor
@@ -481,14 +483,29 @@ def process(args):
         print(f"Fetching split {split}...")
         dataset = CoVoST(root, split, args.src_lang, args.tgt_lang, args.pair_type)
         print("Extracting log mel filter bank features...")
-        fbank_pool = multiprocessing.Pool(multiprocessing.cpu_count()-1)
+        if split == 'train' and args.cmvn_type == "global":
+            gcmvn_feature_list = []
+            print("And estimating cepstral mean and variance stats...", flush=True)
+
+
+        #fbank_pool = multiprocessing.Pool(multiprocessing.cpu_count()-1)
         for waveform, sample_rate, _, _, _, utt_id in tqdm(dataset):
-            fbank_pool.apply_async(extract_fbank_features, args=(waveform, sample_rate, feature_root / f"{utt_id}.npy"))
-            #extract_fbank_features(
-            #    waveform, sample_rate, feature_root / f"{utt_id}.npy"
-            #)
-        fbank_pool.close()
-        fbank_pool.join()
+            #fbank_pool.apply_async(extract_fbank_features, args=(waveform, sample_rate, feature_root / f"{utt_id}.npy"))
+            features = extract_fbank_features(
+                waveform, sample_rate, feature_root / f"{utt_id}.npy"
+            )
+            if split == 'train' and args.cmvn_type == "global":
+                if (len(gcmvn_feature_list) < args.gcmvn_max_num) and (features is not None):
+                    gcmvn_feature_list.append(features)
+        #fbank_pool.close()
+        #fbank_pool.join()
+        
+        if split == 'train' and args.cmvn_type == "global":
+            # Estimate and save cmv
+            stats = cal_gcmvn_stats(gcmvn_feature_list)
+            with open(root / "gcmvn.npz", "wb") as f:
+                np.savez(f, mean=stats["mean"], std=stats["std"])
+
     # Pack features into ZIP
     zip_path = root / "fbank80.zip"
     print("ZIPing features...")
@@ -535,7 +552,11 @@ def process(args):
         root,
         spm_filename=spm_filename_prefix + ".model",
         yaml_filename=f"config_{task}.yaml",
-        specaugment_policy="lb",
+        specaugment_policy="st",
+        cmvn_type=args.cmvn_type,
+        gcmvn_path=(
+            root / "gcmvn.npz" if args.cmvn_type == "global" else None
+        ),
     )
     # Clean up
     shutil.rmtree(feature_root)
@@ -554,6 +575,14 @@ def main():
         type=str,
         choices=["bpe", "unigram", "char"],
     ),
+    parser.add_argument("--cmvn-type", default="utterance",
+                        choices=["global", "utterance"],
+                        help="The type of cepstral mean and variance normalization")
+    parser.add_argument("--gcmvn-max-num", default=150000, type=int,
+                        help=(
+                            "Maximum number of sentences to use to estimate"
+                            "global mean and variance"
+                            ))
     parser.add_argument("--vocab-size", default=1000, type=int)
     parser.add_argument("--src-lang", "-s", required=True, type=str)
     parser.add_argument("--tgt-lang", "-t", type=str)