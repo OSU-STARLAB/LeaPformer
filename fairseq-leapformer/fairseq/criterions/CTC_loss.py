3869130412d8fe920d71c5be006f0fc6f07bbae2
Meaffel <raffelm@oregonstate.edu>
2023-03-08 19:23:35 -0800

    Fixed importing error.

--- fairseq-leapformer/fairseq/criterions/CTC_loss.py
+++ fairseq-leapformer/fairseq/criterions/CTC_loss.py
@@ -13,7 +13,6 @@ import torch
 import torch.nn.functional as F
 from fairseq import utils
 from fairseq.criterions import FairseqCriterion
-from examples.speech_recognition.utils.wer_utils import Code, EditDistance, Token
 
 USE_CUDA_EDIT_DISTANCE=False
 if torch.cuda.is_available():
@@ -29,7 +28,7 @@ logger.setLevel(logging.DEBUG)
 
 
 def arr_to_toks(arr):
-    
+    from examples.speech_recognition.utils.wer_utils import Token
     toks = []
     for a in arr:
         toks.append(Token(str(a), 0.0, 0.0))
@@ -37,7 +36,7 @@ def arr_to_toks(arr):
 
 
 def compute_ctc_uer(logprobs, targets, input_lengths, target_lengths, blank_idx):
-    from examples.speech_recognition.utils.wer_utils import Code, EditDistance, Token
+    from examples.speech_recognition.utils.wer_utils import Code, EditDistance
     """
         Computes utterance error rate for CTC outputs
 
