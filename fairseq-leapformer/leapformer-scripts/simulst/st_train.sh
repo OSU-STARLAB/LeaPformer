70d0105b28bd2403543cc45ae2dda9b34a5d5369
Victor Agostinelli <35327788+agostinv@users.noreply.github.com>
2024-09-11 23:22:30 -0700

    Update st_train.sh

--- fairseq-leapformer/leapformer-scripts/simulst/st_train.sh
+++ fairseq-leapformer/leapformer-scripts/simulst/st_train.sh
@@ -26,6 +26,7 @@ fairseq-train ${MUSTC_ROOT_LIN}/en-zh \
     --max-update 100000 --seed 2 --ddp-backend legacy_ddp --log-interval 50 \
     --encoder-normalize-before --decoder-normalize-before --share-decoder-input-output-embed \
     --encoder-attention-heads 8 --decoder-attention-heads 8 \
+    --leap-factor 4 \
     --enc-leapformer-enable \
     --dec-sa-leapformer-enable \
     --dec-ca-leapformer-enable \
