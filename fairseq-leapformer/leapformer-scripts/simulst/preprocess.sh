861ff61f38f2650838bf885bfdcea0bdfb313511
Victor Agostinelli <35327788+agostinv@users.noreply.github.com>
2024-09-16 15:05:30 -0700

    Update preprocess.sh, aligned formatting with other scripts

--- fairseq-leapformer/leapformer-scripts/simulst/preprocess.sh
+++ fairseq-leapformer/leapformer-scripts/simulst/preprocess.sh
@@ -21,22 +21,22 @@ export PYTHONPATH='${PYTHONPATH}:.'
 
 # assumes that tar ball of MuST-C data is already placed where it should be and unzipped
 # we are assuming en-de as the language pair of interest, CoVoST is fr-en
-python ${FAIRSEQ_ROOT_LIN}/examples/speech_to_text/prep_mustc_data.py \
-      --data-root ${FAIRSEQ_ROOT}/${MUSTC_ROOT} --task asr  --langs-to-process de \
+python ${FAIRSEQ_ROOT}/examples/speech_to_text/prep_mustc_data.py \
+      --data-root ${MUSTC_ROOT} --task asr  --langs-to-process de \
       --vocab-type unigram --vocab-size 10000 \
       --cmvn-type global
 
-python ${FAIRSEQ_ROOT_LIN}/examples/speech_to_text/prep_mustc_data.py \
-      --data-root ${FAIRSEQ_ROOT}/${MUSTC_ROOT} --task st  --langs-to-process de \
+python ${FAIRSEQ_ROOT}/examples/speech_to_text/prep_mustc_data.py \
+      --data-root ${MUSTC_ROOT} --task st  --langs-to-process de \
       --vocab-type unigram --vocab-size 10000 \
       --cmvn-type global
 
-#python ${FAIRSEQ_ROOT_LIN}/examples/speech_to_text/prep_covost_data.py \
-#      --data-root ${FAIRSEQ_ROOT}/${COVOST_ROOT} --src-lang fr \
+#python ${FAIRSEQ_ROOT}/examples/speech_to_text/prep_covost_data.py \
+#      --data-root ${COVOST_ROOT} --src-lang fr \
 #      --vocab-type unigram --vocab-size 10000 \
 #      --cmvn-type global \
 #
-#python ${FAIRSEQ_ROOT_LIN}/examples/speech_to_text/prep_covost_data.py \
-#      --data-root ${FAIRSEQ_ROOT_LIN}/${COVOST_ROOT} --src-lang fr --tgt-lang en \
+#python ${FAIRSEQ_ROOT}/examples/speech_to_text/prep_covost_data.py \
+#      --data-root ${COVOST_ROOT} --src-lang fr --tgt-lang en \
 #      --vocab-type unigram --vocab-size 10000 \
 #      --cmvn-type global \
